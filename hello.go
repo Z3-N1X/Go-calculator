fdaaf657f43da3df9827e3a201eebe768034e3d7
Z3_N1X <iliya.souri7890@gmail.com>
2024-09-16 17:31:14 +0330

    added +

--- hello.go
+++ hello.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+
+	var x int
+	fmt.Print("Enter the first number:")
+	fmt.Scan(&x)
+	var y int
+	fmt.Print("Enter the second number:")
+	fmt.Scan(&y)
+	fmt.Print(x + y)
+
 }
